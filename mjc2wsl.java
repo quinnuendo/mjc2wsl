70a6333f9c21f5c9f313d83d7ef06a3b2dfe1a56
Doni Pracner <quinnuendo@gmail.com>
2012-09-01 22:46:29 +0200

    replaced a14 with a15 as it should be

--- mjc2wsl.java
+++ mjc2wsl.java
@@ -183,7 +183,7 @@ public class mjc2wsl{
 		for (int i=0;i<14;i++) get();
 		
 		prl(getStandardStart());
-		prl("SKIP;\n ACTIONS A_S_start:\n A_S_start == CALL a14 END");
+		prl("SKIP;\n ACTIONS A_S_start:\n A_S_start == CALL a15 END");
 		int op = get();
 		while (op>=0){
 				if (originalInComments) prl(createComment(""+op,C_OC));
